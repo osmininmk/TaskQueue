fdc5d69f33ce1b821d66bf889f7fe9060cc519b8
Osminin Mark <mosmin@mera.ru>
2014-07-08 09:45:49 +0400

    Add hibernate config

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,8 +1,45 @@
 package main;
 
+import org.apache.log4j.Logger;
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.Session;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import services.CleanupJob;
+import services.Executor;
+import services.Generator;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.Timer;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 public class Main {
-	
+    private static final SessionFactory ourSessionFactory;
+    static {
+        try {
+            Configuration configuration = new Configuration();
+            configuration.configure("hibernate.cfg.xml");
+			ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+            ourSessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        } catch (Throwable ex) {
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+    /**
+     * Obtain session from sessionFactory, thread safe
+     * @return  hibernate session
+     * @exception  HibernateException
+     */
+    public static Session getSession() throws HibernateException {
+        return ourSessionFactory.openSession();
+    }
+
     public static void main(final String[] args) throws Exception {
-		
 	}
 }
